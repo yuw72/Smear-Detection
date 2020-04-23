83adac4d5050fe5533e917fff76e27a8771b7b9c
Your Name <you@example.com>
2020-04-23 13:57:57 -0500

    'gaussian'

--- code/average_img.py
+++ code/average_img.py
@@ -4,20 +4,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def average_image(path, shape, save):
+def average_image(path, shape, save, N):
+
     images = os.listdir(path)
     aggregate = np.zeros(shape)
     filename = 'average_img_cam' + path[-1] + '.jpg'
-    for image in images:
-        aggregate += cv2.imread(os.path.join(path, image), cv2.IMREAD_COLOR)
 
-    average = aggregate / len(images)
+    index = np.random.choice(len(images)-N, 1, replace=False)  # randomly sample N continuous photo from all images
+    # print(index)
+
+    for i in range(N):
+        aggregate += cv2.imread(os.path.join(path, images[index[0]+i]), cv2.IMREAD_COLOR)
+
+    average = aggregate / N
     cv2.imwrite(os.path.join(save, filename), average)
-
+    print('saved!')
 
 if __name__ == '__main__':
 
-    img_shape = (2032, 2032, 3)
+    img_shape = (2032, 2032, 3)  # shape of the image
+    num = 200  # number of samples to take
 
     # pls change to your own directory
     path_to_data = 'D:/Northwestern University/2020Spring/Geospatial Visualization/HW/HW1/data/'
@@ -28,4 +34,4 @@ if __name__ == '__main__':
     for cam in cams:
         path_to_cam = os.path.join(path_to_data, cam)
         # print(path_to_cam)
-        average_image(path_to_cam, img_shape, save_path)
+        average_image(path_to_cam, img_shape, save_path, num)
---
+++ code/gaussian_blur.py
@@ -0,0 +1,27 @@
+import cv2
+import matplotlib.pyplot as plt
+import os
+
+
+def gaussian_blur(path, k_size, sigma, save):
+    images = os.listdir(path)
+    i = 0
+    for image in images:
+        if image.endswith('.jpg'):
+            img = cv2.imread(os.path.join(path, image), cv2.IMREAD_COLOR)
+
+            img = cv2.GaussianBlur(img, k_size, sigma)
+            cv2.imwrite(os.path.join(save, f'gaussian_blur{i}.jpg'), img)
+            i += 1
+
+
+if __name__ == '__main__':
+    kernel_size = (5, 5)
+    sigma = 1
+
+    path_to_data = 'D:/Northwestern University/2020Spring/Geospatial ' \
+                   'Visualization/HW/HW1/Smear-Detection/results/average_image'
+    save_path = 'D:/Northwestern University/2020Spring/Geospatial ' \
+                'Visualization/HW/HW1/Smear-Detection/results/gaussian_blur'
+
+    gaussian_blur(path_to_data, kernel_size, sigma, save_path)
